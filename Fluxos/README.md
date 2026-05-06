57769fca12f709f9ebc3fcc744ce235b7364b96c
ju-vargas <ju.rodrigues0100@gmail.com>
2026-05-06 14:08:02 -0300

    updates readme and complets report

---
+++ Fluxos/README.md
@@ -0,0 +1,17 @@
+### Como rodar os experimentos
+
+```
+mkdir build
+cd build
+cmake ..
+make experiments
+./run_experiments.pl
+```
+
+### Como todar os testes
+```
+mkdir build
+cd build
+cmake ..
+make run_tests
+```
--- Fluxos/build/CMakeFiles/experiments.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/experiments"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/experiments.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
